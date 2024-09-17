eab697b5a9b1e1e128899d9fd070cbb6ccf95a95
umairahmed88 <umairahmed_88@ymail.com>
2024-09-17 21:18:33 +0500

    updated auth routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,6 +7,7 @@ import {
 	updateUser,
 } from "../controllers/auth.controllers.js";
 import { verifyToken } from "../utils/verifyAuth.js";
+import { verifyEmail } from "../utils/verifyEmail.js";
 
 const router = express.Router();
 
@@ -15,5 +16,6 @@ router.post("/signin", signin);
 router.post("/signin-google", google);
 router.put("/update-user/:id", verifyToken, updateUser);
 router.post("/signout", verifyToken, signout);
+router.get("verify-email", verifyEmail);
 
 export default router;
