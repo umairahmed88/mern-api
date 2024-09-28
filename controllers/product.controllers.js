0a01683b76859deec905f8284ce36ed8a40d26b4
umairahmed88 <umairahmed_88@ymail.com>
2024-09-29 04:40:57 +0500

    updated product update controller

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -33,8 +33,25 @@ export const getAllProducts = async (req, res) => {
 						totalProducts === 1
 							? `There is ${totalProducts} product available`
 							: `There are ${totalProducts} available`,
+					products,
 			  })
 			: res.status(404).json({ message: "There is no available product" });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+};
+
+export const updateProduct = async (req, res) => {
+	try {
+		const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+		});
+
+		return product
+			? res
+					.status(200)
+					.json({ message: `Product ${product.name} updated`, product })
+			: res.status(404).json({ message: `${product.name} not found` });
 	} catch (err) {
 		res.status(500).json({ message: err.message });
 	}
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -3,6 +3,7 @@ import {
 	createProduct,
 	getAllProducts,
 	getOne,
+	updateProduct,
 } from "../controllers/product.controllers.js";
 import { verifyToken } from "../utils/verifyAuth.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/create-product", verifyToken, createProduct);
 router.get("/get-one-product/:id", getOne);
 router.get("/get-all-products", getAllProducts);
+router.put("/update-product/:id", updateProduct);
 
 export default router;
