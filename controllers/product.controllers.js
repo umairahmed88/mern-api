289ace968b6d278fa9be2f9e3fadbea3b5e98944
umairahmed88 <umairahmed_88@ymail.com>
2024-09-29 14:57:37 +0500

    updated product controller to get all products

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -16,7 +16,7 @@ export const getOne = async (req, res) => {
 
 		return product
 			? res.status(200).json(product)
-			: res.status(404).json({ message: "Product is not available" });
+			: res.status(404).json({ message: `${product.name} not available` });
 	} catch (err) {
 		res.status(500).json({ message: err.message });
 	}
@@ -51,7 +51,7 @@ export const updateProduct = async (req, res) => {
 			? res
 					.status(200)
 					.json({ message: `Product ${product.name} updated`, product })
-			: res.status(404).json({ message: `${product.name} not found` });
+			: res.status(404).json({ message: `${product.name} not available` });
 	} catch (err) {
 		res.status(500).json({ message: err.message });
 	}
