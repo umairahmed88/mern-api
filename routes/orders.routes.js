be21a94e49b01af8faf9d4ad5ce3cc732749055b
umairahmed88 <umairahmed_88@ymail.com>
2024-10-24 09:26:46 +0500

    added fetch order, fetch orders delete order and delete orders routes

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -1,9 +1,19 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyAuth.js";
-import { createOrder } from "../controllers/orders.controller.js";
+import {
+	createOrder,
+	deleteAllOrders,
+	deleteOrder,
+	fetchAllOrders,
+	fetchOneOrder,
+} from "../controllers/orders.controller.js";
 
 const router = express.Router();
 
 router.post("/create-order", verifyToken, createOrder);
+router.get("/get-all", verifyToken, fetchAllOrders);
+router.get("/get-one/:id", verifyToken, fetchOneOrder);
+router.delete("/delete-one/:id", verifyToken, deleteOrder);
+router.delete("/delete-all", verifyToken, deleteAllOrders);
 
 export default router;
